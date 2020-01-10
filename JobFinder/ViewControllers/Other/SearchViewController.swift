30e7e0e032549e95b274376d8348d22eba95be46
Pavlin Panayotov <panayotov1995@gmail.com>
2020-01-10 21:41:26 +0200

    Unstaged files from previous commit - "Added search by contract type. Fix bug with `NibView`.".

--- JobFinder/ViewControllers/Other/SearchViewController.swift
+++ JobFinder/ViewControllers/Other/SearchViewController.swift
@@ -43,6 +43,8 @@ final class SearchViewController: ScrollViewController {
 		return pickerView
 	}()
 	
+	private lazy var contractTypePicker = Dropdown()
+	
 	private let locationPickerView = LocationPickerView()
 	
 	override func viewDidLoad() {
@@ -52,10 +54,12 @@ final class SearchViewController: ScrollViewController {
 		scrollSubviewsInsets = UIEdgeInsets(inset: 20)
 		
 		setupLocationPickerView()
+		setupContractTypeDropdown()
 		
 		addScrollSubviews(
 			textField,
 			salaryPickerView,
+			contractTypePicker,
 			locationPickerView,
 			searchButton
 		)
@@ -64,10 +68,15 @@ final class SearchViewController: ScrollViewController {
 	// MARK: - Actions
 	@objc
 	private func searchButtonTapped(_ sender: Any) {
+		hideKeyboard()
+		
 		let filter = Filter(
 			text: textField.text,
 			minimumSalary: salaryPickerView.minimumSalary,
-			region: locationPickerView.selectedRegion
+			region: locationPickerView.selectedRegion,
+			contractType: ContractType(
+				optionalRawValue: contractTypePicker.selectedOption
+			)
 		)
 		let results = DataManager.shared.jobs(forFilter: filter)
 		
@@ -81,12 +90,18 @@ final class SearchViewController: ScrollViewController {
 	private func setupLocationPickerView() {
 		locationPickerView.regions = DataManager.shared.regions.map { $0.name }
 	}
+	
+	private func setupContractTypeDropdown() {
+		contractTypePicker.title = "Тип на договора"
+		contractTypePicker.placeholder = "Избери"
+		contractTypePicker.options = ContractType.allCases.map { $0.title } + ["всички"]
+	}
 }
 
 // MARK: UITextFieldDelegate
 extension SearchViewController: UITextFieldDelegate {
 	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-		textField.resignFirstResponder()
+		hideKeyboard()
 		return true
 	}
 }
